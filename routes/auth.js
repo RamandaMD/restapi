5bff91cd8f3a5251534a60eae28e1f3a74d2b84a
RamandaMD <ramandamd@mhs.mdp.ac.id>
2023-05-31 11:29:47 +0700

    up

--- routes/auth.js
+++ routes/auth.js
@@ -14,17 +14,57 @@ function result(succ, msg, details) {
             success: succ,
             message: msg,
             data: details
-        }else {
-            return {
-                success: succ,
-                message: msg
+        }
+    }else {
+        return  {
+            success: succ,
+            message: msg
         }
     }
-}
 }
 
 //register
 router.post('/register', async (req, res) => {
-    const (error) = registerValidation(req.body)
+    const {error} = registerValidation(req.body)
     if (error) return res.status(200).json(result(0, error.details[0].message))
+
+    //username exist
+    const usernameExist = await User.findOne({username: req, password: req.body.username})
+    if(usernameExist) return res.status(200).json (result(0, 'Username already exist'))
+
+    //hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(req.body.password, salt)
+
+    const user = new User({
+        username: req.body.username,
+        password: hashPassword
+    })
+
+    try {
+        const saveUser = await user.save()
+        res.status(200).json(result(1, 'Register User Success!', saveUser))
+    } catch (error) {
+        res.status(200).json(result(0, 'Register User Failed!'))
+    }
 })
+
+//Login
+router.post('/login', async(req, res) => {
+    const {error} = loginValidation(req.body)
+    if (error) return res.status(200).json(result(0, error.details[0].message))
+
+    //username exist
+    const user = await User.findOne({
+        username: req.body.username
+    })
+    if (!user) return res.status(200).json(result(0, 'Your username is not registered!'))
+
+    // check password
+    const validPwd = await bcrypt.compare(req.body.password, user.passowrd)
+    if (!validPwd) return res.status(200).json(result(0, 'Your password is wrong!'))
+
+    return res.status(200).json(result(0, 'Login User Success', user))
+})
+
+module.exports = router
